bc77d8d388ff73e3a7c249b9f58e60bd40e74027
George <64723717+SURPRlSE@users.noreply.github.com>
2021-01-29 14:09:56 +0800

    上传文件
    第一次上传文件的描述

---
+++ plus_test.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+//2021年1月26日
+//功能：无
+//目的：测试自增运算符和逗号表达式；
+
+int main(void)
+{
+    int i,j,k,t;
+
+//    i = j = 3;
+//    k = i++;
+//    t = ++j;
+//
+//    printf("i = %d;j = %d;k = %d;t = %d",i,j,k,t);
+//
+//    i = 4;j = 4;k = 3;t = 4
+
+
+
+//    int i;
+//    int j = 2;
+//
+//    i = (j++,++j,j+2,j-3);
+//
+//    printf("i = %d;",i);
+//    i = 1;
+
+
+//    int sum = 0;
+//    int i = 1;
+//
+//    while (i <= 100)
+//    {
+//        sum += i;
+//        i++;
+//    }
+//    printf("sum = %d",sum);
+
+    return 0;
+}
